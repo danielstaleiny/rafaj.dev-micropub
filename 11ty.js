ff8375309485b4d375497c0b1df884cad88ecf2c
danielstaleiny <danielstaleiny@tutanota.com>
2021-02-18 12:00:56 +0100

    take full date,

--- 11ty.js
+++ 11ty.js
@@ -157,7 +157,7 @@ export const EleventyPreset = class {
       posttype: properties['post-type']
         ? properties['post-type'].toLowerCase()
         : 'notype',
-      date: properties.published.substring(0, 10), // substring to take only published date
+      date: properties.published, // substring to take only published date
       ...(properties.name && { title: properties.name }),
       ...(properties.summary && {
         description: properties.summary || properties.name || '',
